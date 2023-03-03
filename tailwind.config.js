5f94d191fe51426f7e6c952969e11c32f94db68d
mizucode <mizurisysthensis3@gmail.com>
2023-03-03 11:31:37 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#FFD4D4",
+                secondary: "#FFFFE8",
+                third: "#CDE990",
+                huruf: "#AACB73",
+            },
+        },
+    },
+    plugins: [],
+};
